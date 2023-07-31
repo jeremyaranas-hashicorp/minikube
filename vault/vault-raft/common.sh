a43c30faf91aee0800da0dfba51adbe0870819e0
Jeremy Aranas <jeremy.aranas@hashicorp.com>
2023-07-31 11:56:41 -0700

    refactored k8s_auth_method to use a script and common.sh

--- vault/vault-raft/common.sh
+++ vault/vault-raft/common.sh
@@ -67,8 +67,8 @@ configure_k8s_auth_role () {
     echo "INFO: Configuring k8s auth method role"
     # Create k8s auth role to connect k8s service account, k8s namespace, Vault policy
     kubectl exec -ti vault-0 -- vault write auth/kubernetes/role/test-role \
-        bound_service_account_names=test-sa \
-        bound_service_account_namespaces=default \
+        bound_service_account_names=test-sa,vault \
+        bound_service_account_namespaces=default,test-namespace \
         policies=test-policy \
         ttl=24h
     # Create k8s service account
--- vault/vault-raft/k8s_auth_method/cleanup.sh
+++ vault/vault-raft/k8s_auth_method/cleanup.sh
@@ -1,8 +1,7 @@
 # Cleanup k8s auth method resources
-kubectl delete sa -n test test-cloud
-kubectl delete namespace test
-kubectl delete sa vault-auth
-kubectl delete clusterrolebindings.rbac.authorization.k8s.io role-tokenreview-binding
+kubectl delete namespace test-namespace
+kubectl delete sa test-sa
+kubectl delete clusterrolebindings.rbac.authorization.k8s.io token-review-clusterrolebindings
 kubectl exec -ti vault-0 -- vault auth disable kubernetes
 
 # Uninstall Vault Helm chart
@@ -12,7 +11,7 @@ helm uninstall vault
 kubectl delete pvc -l app.kubernetes.io/instance=vault 
 
 # Remove files
-rm -f *keys.json
+rm -f init.json
 
 # Remove license
 kubectl delete secrets vault-ent-license
