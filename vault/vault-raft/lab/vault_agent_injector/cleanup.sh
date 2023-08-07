dba7a97d0eaf082c0e29512c4ed2f775daa1113d
Jeremy Aranas <jeremy.aranas@hashicorp.com>
2023-08-07 10:26:34 -0700

    added cleanup script

---
+++ vault/vault-raft/lab/vault_agent_injector/cleanup.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# Delete sa
+kubectl delete sa test-sa
+
+# Delete deployment
+kubectl delete deployment nginx-deployment
+
+# Uninstall Vault Helm chart
+helm uninstall vault
+
+# Remove PVCs
+kubectl delete pvc -l app.kubernetes.io/instance=vault
+
+# Remove files
+rm -f init.json
+
+# Remove license
+kubectl delete secrets vault-ent-license
