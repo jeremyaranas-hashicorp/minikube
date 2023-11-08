ce84b9754d121203ed86b3f0d734c7bc9d6c15aa
Jeremy Aranas <jeremy.aranas@hashicorp.com>
2023-11-07 16:34:44 -0800

    removed old dir

--- vault/.vault-raft/transit_auto_unseal/vault-init.sh
+++
@@ -1,9 +0,0 @@
-# Vault cluster
-
-source ../common.sh
-
-helm install vault hashicorp/vault --values vault-values.yaml
-set_ent_license
-init_vault_using_auto_unseal
-sleep 10
-
--- vault/.vault-raft/vault_k8s_cluster/init.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-source ../common.sh
-
-set_ent_license
-init_vault
-unseal_vault
-add_nodes_to_cluster
