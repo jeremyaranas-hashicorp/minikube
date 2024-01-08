fde21aa9a8f64a54634f90aaa77fd5176d468ae5
Jeremy Aranas <jeremy.aranas@hashicorp.com>
2024-01-08 09:56:23 -0800

    updated VSO to use vault-connection

--- vault/vault-reproductions/scripts/vault-secrets-operator.sh
+++ vault/vault-reproductions/scripts/vault-secrets-operator.sh
@@ -7,13 +7,10 @@ source ../main/common.sh
 kubectl exec -ti -n vault vault-0 -- vault write auth/kubernetes/config kubernetes_host="https://10.96.0.1:443"
 
 # Install Vault Secrets Operator Helm chart 
-helm install vault-secrets-operator hashicorp/vault-secrets-operator --version 0.3.3 -n vault-secrets-operator-system --create-namespace --values ../helm_chart_values_files/vault-operator-values.yaml
+helm install vault-secrets-operator hashicorp/vault-secrets-operator -n vault-secrets-operator-system --create-namespace --values ../helm_chart_values_files/vault-operator-values.yaml
 
 # Create a namespace for the k8s secret
 kubectl create ns vso
 
 # Set up k8s auth method for the secret
-kubectl apply -f ../manifests/vso-vault-auth.yaml
-
-# Create the secret 
-kubectl apply -f ../manifests/vso-static-secret.yaml
+kubectl apply -f ../manifests/vso-custom-resources.yaml
