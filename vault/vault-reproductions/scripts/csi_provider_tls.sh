826a4a6ca785664cc7def1da4c81aee8c41278f3
Jeremy Aranas <jeremy.aranas@hashicorp.com>
2024-01-09 09:14:01 -0800

    fixed csi for tls

---
+++ vault/vault-reproductions/scripts/csi_provider_tls.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+source ../main/common.sh
+./k8s_auth.sh
+configure_test_secrets_engine
+enable_k8s_auth
+set_vault_policy
+configure_k8s_auth_role
+
+# Install secrets store CSI driver
+helm repo add secrets-store-csi-driver https://kubernetes-sigs.github.io/secrets-store-csi-driver/charts
+helm install -n vault csi secrets-store-csi-driver/secrets-store-csi-driver \
+    --set syncSecret.enabled=true
+
+# Create the SecretProviderClass
+kubectl apply --filename ../manifests/csi-secret-provider-class-tls.yaml
+
+# Create application pod
+kubectl apply --filename ../manifests/csi-app-pod_tls.yaml
